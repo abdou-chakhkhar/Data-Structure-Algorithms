7f07fdce13b11dde8c5f6c33f0e5e949a0c1a7f6
abdou-chakhkhar <67806530+abdou-chakhkhar@users.noreply.github.com>
2022-09-07 14:32:28 +0100

    Update BinarySearchTree.js

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -153,3 +153,21 @@ function traverse(node) {
   tree.right = node.right === null ? null : traverse(node.right);
   return tree;
 }
+
+//If you know a solution is not far from the root of the tree:
+// => BFS
+
+//If the tree is very deep and solutions are rare, 
+// => BFS (DFS will take long time. )
+
+//If the tree is very wide:
+// => DFS (BFS will need too much memory)
+
+//If solutions are frequent but located deep in the tree
+// => DFS
+
+//determining whether a path exists between two nodes
+// => DFS
+
+//Finding the shortest path
+// => BFS
